f10cede06a4e0f37d6ab166e7b7843dfd7cfff03
Eduardo Flores <edfloreshz@gmail.com>
2023-06-04 21:42:29 -0700

    Make every field public

--- src/body.rs
+++ src/body.rs
@@ -3,14 +3,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Body {
-	content: String,
-	content_type: BodyType,
+    pub content: String,
+    pub content_type: BodyType,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(rename_all = "camelCase")]
 pub enum BodyType {
-	#[default]
-	Text,
-	Html,
+    #[default]
+    Text,
+    Html,
 }
--- src/recurrence.rs
+++ src/recurrence.rs
@@ -4,75 +4,75 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct Recurrence {
-	pub pattern: RecurrencePattern,
-	pub range: RecurrenceRange,
+    pub pattern: RecurrencePattern,
+    pub range: RecurrenceRange,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct RecurrenceRange {
-	end_date: NaiveDate,
-	number_of_occurrences: i32,
-	recurrence_time_zone: String,
-	start_date: NaiveDate,
-	#[serde(rename = "type")]
-	recurrence_range_type: RecurrenceRangeType,
+    pub end_date: NaiveDate,
+    pub number_of_occurrences: i32,
+    pub recurrence_time_zone: String,
+    pub start_date: NaiveDate,
+    #[serde(rename = "type")]
+    pub recurrence_range_type: RecurrenceRangeType,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub enum RecurrenceRangeType {
-	#[default]
-	EndDate,
-	NoEnd,
-	Numbered,
+    #[default]
+    EndDate,
+    NoEnd,
+    Numbered,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct RecurrencePattern {
-	pub day_of_month: i32,
-	pub days_of_week: Vec<DayOfWeek>,
-	pub first_day_of_week: DayOfWeek,
-	pub index: Option<WeekIndex>,
-	pub interval: i32,
-	pub month: i32,
-	#[serde(rename = "type")]
-	pub recurrence_pattern_type: RecurrencePatternType,
+    pub day_of_month: i32,
+    pub days_of_week: Vec<DayOfWeek>,
+    pub first_day_of_week: DayOfWeek,
+    pub index: Option<WeekIndex>,
+    pub interval: i32,
+    pub month: i32,
+    #[serde(rename = "type")]
+    pub recurrence_pattern_type: RecurrencePatternType,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub enum DayOfWeek {
-	#[default]
-	Sunday,
-	Monday,
-	Tuesday,
-	Wednesday,
-	Thursday,
-	Friday,
-	Saturday,
+    #[default]
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub enum WeekIndex {
-	#[default]
-	First,
-	Second,
-	Third,
-	Fourth,
-	Last,
+    #[default]
+    First,
+    Second,
+    Third,
+    Fourth,
+    Last,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 #[serde(rename_all = "camelCase")]
 pub enum RecurrencePatternType {
-	#[default]
-	Daily,
-	Weekly,
-	AbsoluteMonthly,
-	RelativeMonthly,
-	AbsoluteYearly,
-	RelativeYearly,
+    #[default]
+    Daily,
+    Weekly,
+    AbsoluteMonthly,
+    RelativeMonthly,
+    AbsoluteYearly,
+    RelativeYearly,
 }
