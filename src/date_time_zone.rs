25ab4302dbc11a0da7aa0ab948cba289b3f895fa
Eduardo Flores <edfloreshz@gmail.com>
2023-06-08 07:03:28 -0700

    Date conversions

--- src/date_time_zone.rs
+++ src/date_time_zone.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
@@ -5,4 +8,19 @@ use serde::{Deserialize, Serialize};
 pub struct DateTimeTimeZone {
     pub date_time: String,
     pub time_zone: String,
+}
+
+impl From<DateTimeTimeZone> for DateTime<Utc> {
+    fn from(date: DateTimeTimeZone) -> Self {
+        DateTime::<Utc>::from_str(&date.date_time).unwrap()
+    }
+}
+
+impl From<DateTime<Utc>> for DateTimeTimeZone {
+    fn from(date: DateTime<Utc>) -> Self {
+        Self {
+            date_time: date.to_string(),
+            time_zone: date.timezone().to_string(),
+        }
+    }
 }
